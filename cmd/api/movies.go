700121fe6db142db139f5e88d00a3c260e3369f8
van9md <van9md@gmail.com>
2025-10-08 22:00:17 +0300

    Added movie struct

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/van9md/greenlight/internal/data"
 )
 
 func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +18,17 @@ func (app *application) showMoviesHandler(w http.ResponseWriter, r *http.Request
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "Show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Deadpool",
+		Runtime:   169,
+		Genres:    []string{"comedy", "action"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
