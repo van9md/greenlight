c613d67d9460343aa027fb342dc44faf528cd103
van9md <van9md@gmail.com>
2025-10-08 14:46:11 +0300

    Added writeJSON helper

--- cmd/api/healthckeck.go
+++ cmd/api/healthckeck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -11,13 +10,10 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered problem and could not process your request", http.StatusInternalServerError)
 		return
 	}
-	js = append(js, '\n')
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
@@ -14,4 +16,16 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
