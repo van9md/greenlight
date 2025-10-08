f775f4fbcd1b7064ff38b232501cb0597aaa6625
van9md <van9md@gmail.com>
2025-10-08 14:38:29 +0300

    Map to json

--- cmd/api/healthckeck.go
+++ cmd/api/healthckeck.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
