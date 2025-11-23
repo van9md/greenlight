669dc584b31fb5e91cce72c1f7259dece45aa3f0
van9md <van9md@gmail.com>
2025-11-23 13:39:33 +0300

    Add paginator metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -44,3 +44,24 @@ func (f Filters) limit() int {
 func (f Filters) offset() int {
 	return (f.Page - 1) * f.PageSize
 }
+
+type Metadata struct {
+	CurrentPage  int `json:"currentpage,omitzero"`
+	PageSize     int `json:"page_size,omitzero"`
+	FirstPage    int `json:"first_page,omitzero"`
+	LastPage     int `json:"last_page,omitzero"`
+	TotalRecords int `json:"total_records,omitzero"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
