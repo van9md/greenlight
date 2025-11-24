8b1db6af078491752c491ca2dce1dd725d28b693
van9md <van9md@gmail.com>
2025-11-24 10:20:21 +0300

    Add rate limiter,rate limiter config

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -20,11 +23,41 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+	type client struct {
+		lastSeen time.Time
+		limiter  *rate.Limiter
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
-			app.rateLimitExceedResponse(w, r)
-			return
+		if app.config.limiter.enabled {
+			ip := realip.FromRequest(r)
+			mu.Lock()
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst)}
+			}
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceedResponse(w, r)
+				return
+			}
+			clients[ip].lastSeen = time.Now()
+			mu.Unlock()
 		}
 		next.ServeHTTP(w, r)
 	})
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.25.0
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
 	golang.org/x/time v0.14.0 // indirect
 )
