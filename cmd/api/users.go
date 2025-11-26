231276e3dbfc6825ca34924e46c179f87de4d230
van9md <van9md@gmail.com>
2025-11-26 11:03:07 +0300

    Add sending activation code to mail

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"errors"
+	"net/http"
+	"time"
+
 	"github.com/van9md/greenlight/internal/data"
 	"github.com/van9md/greenlight/internal/validator"
-	"net/http"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,9 +49,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
