ae5268dc1af6641aaf111746d95e26f6570c90b5
van9md <van9md@gmail.com>
2025-11-26 09:58:25 +0300

    Add Gracefull shutdown of background processes

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -29,7 +29,13 @@ func (app *application) serve() error {
 		app.logger.Info("shutting down server", "signal", s.String())
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+		app.logger.Info("completing background tasks", "addr", srv.Addr)
+		app.wg.Wait()
+		shutdownError <- err
 	}()
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 	err := srv.ListenAndServe()
