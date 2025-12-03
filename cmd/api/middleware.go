cc28586537cbba076b3255e1c673a184db9d49b5
van9md <van9md@gmail.com>
2025-12-03 20:33:24 +0300

    Add authorization

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/tomasen/realip"
+	"github.com/van9md/greenlight/internal/data"
+	"github.com/van9md/greenlight/internal/validator"
+	"golang.org/x/time/rate"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
-
-	"github.com/tomasen/realip"
-	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -59,6 +62,41 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			clients[ip].lastSeen = time.Now()
 			mu.Unlock()
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidCredentialsResponse(w, r)
+			return
+		}
+		token := headerParts[1]
+		v := validator.New()
+		if data.ValidateTokenPlaintext(v, token); !v.Valid() {
+			app.invalidCredentialsResponse(w, r)
+			return
+		}
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidCredentialsResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+		r = app.contextSetUser(r, user)
 		next.ServeHTTP(w, r)
 	})
 }
